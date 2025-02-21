38d564e2efc249a8a1c6742acf7877791b2423f3
Cyber Bayes <128910495+JOZUJIOJIO@users.noreply.github.com>
2025-02-21 12:44:30 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,8 +10,8 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  STARTER_HERO_TITLE_1: '基于DEEPSEEK结合RPA驱动的私域自动化管理系统', // 英雄区文字
+  STARTER_HERO_TITLE_2: '通过大模型接管微信、睡梦中完成客户成交', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
