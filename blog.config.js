0438cba2a37224d6d8c9d5ba264026ed0d42cb45
Cyber Bayes <128910495+JOZUJIOJIO@users.noreply.github.com>
2025-02-28 11:01:36 +0800

    更新博客配置文件

--- blog.config.js
+++ blog.config.js
@@ -14,8 +14,8 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '曜变 YAO-ION', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个不会代码的科技爱好者', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '曜变', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '不会代码的科技爱好者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://bento.me/cyberbayes', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '人工智能, 智能硬件', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
